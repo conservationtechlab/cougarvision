15aa16a8f13a064d55894dde05eeb3adb03fa772
Katie <kgarwood@sdzwa.org>
2023-04-17 15:41:38 -0700

    Add docstrings and fix some pylint errors
    None of the functions had docstrings to explain the functions
    and there was no module docstring so I added some and fixed some
    other pylint corrections.

--- cougarvision_utils/alert.py
+++ cougarvision_utils/alert.py
@@ -1,9 +1,29 @@
+'''Alert
+
+This script defines two functions, smtp_setup and send_alert. The first function
+creates a server through which one can send an email from a specified account.
+The second function creates the message containing a classified animal of interest
+along with it's label to send to specified emails via the smtp_server created.
+'''
+
 import mimetypes
 from email.message import EmailMessage
 from smtplib import SMTP_SSL, SMTP_SSL_PORT
 
 
 def smtp_setup(username, password, host):
+    '''SMTP Setup
+
+    This function creates a simple mail transfer protocol by taking in a host email,
+    a username and password for an email.
+
+    Args:
+    username: username for email to send message from, string from config
+    password: password for email message will be sent from, string from config
+    host: IMAP protocol to download gmail messages, initialized in detect_img.py
+
+    Returns: SMTP_SSL object logged into the mailing account specified in config yml
+    '''
     # Init sending mail
     smtp_server = SMTP_SSL(host, port=SMTP_SSL_PORT)
     smtp_server.set_debuglevel(1)  # Show SMTP server interactions
@@ -12,6 +32,21 @@ def smtp_setup(username, password, host):
 
 
 def send_alert(alert, conf, img, smtp_server, from_email, to_emails):
+    '''Send Alert
+
+    This function takes in the animal label, the image of the animal of
+    interest, the SMTP server created, and the to and from emails to send
+    the alert containing that specific image along with the confidence value.
+
+    Args:
+    alert: label of animal that the alert is being created for
+    conf: confidence value of the classifier that the animal it says it
+    is is the animal it is
+    img: the PIL.Image of the image that is to be sent, to be converted to binary
+    smtp_server: where the email is to be sent from, SMTP_SSL object
+    from_email: the outgoing address for the alert
+    to_emails: the emails the alert will be sent to, defined in config yml file
+    '''
     # Construct Email Content
     email_message = EmailMessage()
     email_message.add_header('To', ', '.join(to_emails))
