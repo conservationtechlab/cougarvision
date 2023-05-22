9aca9697544d8db521ff016d406f56b53dda0c98
Katie <kgarwood@sdzwa.org>
2023-05-22 16:46:17 -0700

    Move earthranger_utils to pypi package
    All the functions for earthranger integration are now in their
    own package at https://pypi.org/project/earthranger-utils/.
    Still need to update readme and cougarvision dependencies
    to to reflect this package

--- cougarvision_utils/detect_img.py
+++ cougarvision_utils/detect_img.py
@@ -17,9 +17,7 @@ from tensorflow import keras
 from animl import FileManagement, ImageCropGenerator, DetectMD
 from cougarvision_utils.cropping import draw_bounding_box_on_image
 from cougarvision_utils.alert import smtp_setup, send_alert
-from cougarvision_utils.earthranger_utils.post_event_er import post_event
-from cougarvision_utils.earthranger_utils.attach_image_er import attach_image
-from cougarvision_utils.earthranger_utils.post_cougar_log import is_target
+from earthranger_utils import is_target, attach_image, post_event
 
 
 def detect(images, config):
