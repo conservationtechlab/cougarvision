dc2b171c781419b475277b419dfbc6a01f37103e
Katie Demo <kgarwood@sdzwa.org>
2023-10-20 16:22:57 -0700

    Remove inherent white border
    Still a white border if you move the screens over but now once
    they're initialized to their screen they have no white border
    and take up the full screen
    
    Resolves #45

--- demo2.py
+++ demo2.py
@@ -4,13 +4,11 @@ import time
 from screeninfo import get_monitors
 import numpy as np          
 
+
 def get_screen_resolutions():
     monitors = get_monitors()
     resolutions = [(monitor.width, monitor.height) for monitor in monitors]
     return resolutions     
-    
-def create_black_background(height, width):
-    return np.zeros((height, width, 3), dtype="uint8")
                                                            
                                                                                                     
 def get_newest_images(folder_path, num_images):
@@ -30,41 +28,56 @@ def get_newest_images(folder_path, num_images):
     images = [cv2.imread(os.path.join(folder_path, file)) for file in newest_files]
     images = [img for img in images if img is not None]
     return images
-
-                                                                              
+                                                                   
                                                                                                     
-def display_images(images, window_name='CougarVision'):                                            
-    h, w, _ = images[0].shape  
-    # Create a black image with the necessary size
-    display_img = np.zeros((h*3, w*3, 3), np.uint8)                                 
-    for i in range(len(images)):                                                                    
-        x_offset = (i % 3) * w                                                                      
-        y_offset = (i // 3) * h                                                                     
-        display_img[y_offset:y_offset + h, x_offset:x_offset + w] = images[i]                       
+def display_images(images, window_name='CougarVision'): 
+    resolutions = get_screen_resolutions()
+    screen_height = resolutions[0][1]
+    screen_width = resolutions[0][0]
+
+    num_images_per_row = 3
+    num_images_per_col = 3
+
+    max_width_per_image = screen_width // num_images_per_row
+    max_height_per_image = screen_height // num_images_per_col
+
+    display_img = np.zeros((screen_height, screen_width, 3), np.uint8)
+
+    for i, img in enumerate(images):
+        if img is not None:
+            x_offset = (i % num_images_per_row) * max_width_per_image
+            y_offset = (i // num_images_per_row) * max_height_per_image
+
+            resized_image = cv2.resize(img, (max_width_per_image, max_height_per_image))
+
+            display_img[y_offset:y_offset + max_height_per_image, x_offset:x_offset + max_width_per_image] = resized_image
+    
     cv2.imshow(window_name, display_img) 
 
-def display_more_images(images, window_name='Newest Image'):                                          
-    # Check if there are any images to display
-    if not images:
-        print("No images to display")
-        return
+def display_more_images(images, window_name='Newest Image'):  
+    resolutions = get_screen_resolutions()
+    screen_height = resolutions[1][1]
+    screen_width = resolutions[1][0]
 
-    h, w, _ = images[0].shape
+    num_images_per_row = 9 
+    num_images_per_col = 9
 
-    display_img = create_black_background(h*9, w*9)
+    max_width_per_image = screen_width // num_images_per_row
+    max_height_per_image = screen_height // num_images_per_col
 
-    for i in range(len(images)):
-        if images[i] is not None:   
-            x_offset = (i % 9) * w                                                                      
-            y_offset = (i // 9) * h
-            
-            resized_image = cv2.resize(images[i], (w, h))
-            display_img[y_offset:y_offset + h, x_offset:x_offset + w] = resized_image                       
+    display_img = np.zeros((screen_height, screen_width, 3), np.uint8)
+
+    for i, img in enumerate(images):
+        if img is not None:
+            x_offset = (i % num_images_per_row) * max_width_per_image
+            y_offset = (i // num_images_per_row) * max_height_per_image
+
+            resized_image = cv2.resize(img, (max_width_per_image, max_height_per_image))
+
+            display_img[y_offset:y_offset + max_height_per_image, x_offset:x_offset + max_width_per_image] = resized_image
     
     cv2.imshow(window_name, display_img)
-
-
-                                                          
+                                                        
                                                                                                     
 if __name__ == "__main__":                                                                          
     window_name = 'CougarVision'                                                                    
@@ -72,9 +85,6 @@ if __name__ == "__main__":
     
     resolutions = get_screen_resolutions() 
     
-    image1 = np.zeros((resolutions[0][1], resolutions[0][0], 3), np.uint8)
-    image2 = np.zeros((resolutions[0][1], resolutions[0][0], 3), np.uint8) 
-
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
     cv2.namedWindow(newest_window_name, cv2.WINDOW_NORMAL)
 
@@ -85,14 +95,13 @@ if __name__ == "__main__":
     cv2.setWindowProperty(newest_window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)     
                                                                                                     
     while True:     
-        folder_path_for_9 = '/home/katiedemo/demo_images'  # your existing directory
+        folder_path_for_9 = '/home/katiedemo/demo_images'
         num_images = 9
         newest_images = get_newest_images(folder_path_for_9, num_images)
         if len(newest_images) >= 9:
             display_images(newest_images, window_name)
 
-        # For the 81-image display
-        folder_path_for_81 = '/home/katiedemo/unlabeled_photos'  # your new directory for 81 images
+        folder_path_for_81 = '/home/katiedemo/unlabeled_photos'
         number_images = 81
         newester_images = get_newest_images(folder_path_for_81, number_images)                                              
         if len(newester_images) >= 81:
