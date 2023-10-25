2451e45fa35e31bb5e8d9b84bc66ab734a5c1450
Katie Garwood <kgarwood@sdzwa.org>
2023-10-25 13:12:03 -0700

    Add strikeforce auth_token get script
    New users of CougarVision need to obtain an auth_token
    so they can retrieve new images from strikeforce when they
    run fetch_and_alert.py. This script takes in their username
    and password and outputs their auth_token that they'll put
    in the config file

---
+++ cougarvision_utils/strikeforceget.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+username = "<insert strikeforce username here>"
+password = "<insert strikeforce password here>"
+
+
+base = "https://api.strikeforcewireless.com/api/v2/"
+request = "users/sign-in/"
+call = base + request
+body = json.dumps({"user": {"email": username, "password": password}})
+encode = 'json'
+response = requests.post(url=call, data=body, headers={"Content-Type": "application/json"})
+response = response.text
+response = json.loads(response)
+authentication_token = response["meta"]["authentication_token"]
+print(authentication_token)
+
