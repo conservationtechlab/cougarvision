840e2ea8a93c70c5e5dae4a393d0894d63733b87
Katie Garwood <kgarwood@sdzwa.org>
2023-10-26 15:24:49 -0700

    Scrubbed and added docstrings

--- cougarvision_utils/strikeforceget.py
+++ cougarvision_utils/strikeforceget.py
@@ -1,8 +1,9 @@
+'''Script for getting strikeforce auth_token'''
 import requests
 import json
 
-username = "<insert strikeforce username here>"
-password = "<insert strikeforce password here>"
+username = "<insert strikeforce username>"
+password = "<insert strikeforce password>"
 
 
 base = "https://api.strikeforcewireless.com/api/v2/"
@@ -10,9 +11,9 @@ request = "users/sign-in/"
 call = base + request
 body = json.dumps({"user": {"email": username, "password": password}})
 encode = 'json'
-response = requests.post(url=call, data=body, headers={"Content-Type": "application/json"})
+response = requests.post(url=call, data=body,
+                         headers={"Content-Type": "application/json"})
 response = response.text
 response = json.loads(response)
 authentication_token = response["meta"]["authentication_token"]
 print(authentication_token)
-
--- cougarvision_utils/strikeforcegetcameras.py
+++ cougarvision_utils/strikeforcegetcameras.py
@@ -1,23 +1,34 @@
+'''Script for obtaining strikeforce camera IDs'''
 import requests
 import json
 
+
 def getData(base, request, parameters, username, authentication_token):
+    '''Function for retrieving camera info from strikeforce
+    Args:
+        base: strikeforce api link
+        request: cameras
+        parameters: none
+        username: strikeforce username email
+        authentication_token: auth token obtained from strikeforceget.py
+        
+    Returns: 
+        json with all camera info'''
     call = base + request + "?" + parameters
     headers = {"X-User-Email": username, "X-User-Token": authentication_token}
     response = requests.get(call, headers=headers)
     data = response.text
     return json.loads(data)
 
+
 base = "https://api.strikeforcewireless.com/api/v2/"
 request = "cameras"
 parameters = ""
-username = "bioreserve.cam@gmail.com"
-authentication_token = "nWzMPu-ytLvatjyAL7kN"
+username = "<insert username>"
+authentication_token = "<insert auth_token>"
 
 data = getData(base, request, parameters, username, authentication_token)
 pretty_json = json.dumps(data, indent=4)
-print(pretty_json)
-print(data)
 cameras = []
 list_of_cam_info = data['data']
 for i in range(len(list_of_cam_info)):
