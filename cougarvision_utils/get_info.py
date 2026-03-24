f027f0c7da983e9f7f9cdd63b1d1bcaa49f65f3a
Montserrat Jara <jaramontse3@gmail.com>
2026-03-24 12:27:25 -0700

    Changed all attribute names to lower case to meet python snake_case standards.

--- cougarvision_utils/get_info.py
+++ cougarvision_utils/get_info.py
@@ -1,9 +1,13 @@
-
-import argparse
+'''
+ConfigInfo is a class that holds attributes from the configuration
+file (fetch_and_cougar.yml). It expects a configuration file path
+to be provided when initalizing a configInfo object.
+'''
+# import argparse
 import yaml
 
 from animl.classification import load_classifier
-from animl.detection import load_detector 
+from animl.detection import load_detector
 
 class ConfigInfo:
     """ This class is used to define elements from the config file"""
@@ -12,41 +16,36 @@ class ConfigInfo:
     def __init__(self, config_path: str):
 
         with open(config_path, 'r', encoding='utf-8') as stream:
-            self.CONFIG = yaml.safe_load(stream)
-        
-        self.USERNAME = self.CONFIG['username']
-        self.PASSWORD = self.CONFIG['password']
-        self.TOKEN = self.CONFIG['token']
-        self.AUTH = self.CONFIG['authorization']
-        self.CLASSIFIER = self.CONFIG['classifier_model']
-        self.DETECTOR = self.CONFIG['detector_model']
-        self.DEV_EMAILS = self.CONFIG['dev_emails']
-        self.HOST = self.CONFIG['host']
-        self.CLASSES = self.CONFIG['classes']
-        self.MODEL_TYPE = self.CONFIG['detector_model_type']
-        self.CHECKIN_INTERVAL = self.CONFIG['checkin_interval']
-        self.INTERVAL = self.CONFIG['run_scheduler']
-        self.CLASSIFIER_MODEL, self.CLASS_LIST = load_classifier(self.CLASSIFIER, self.CLASSES)
-        self.DETECTOR_MODEL = load_detector(self.DETECTOR, self.MODEL_TYPE)
+            self.config = yaml.safe_load(stream)
+
+        self.username = self.config['username']
+        self.password = self.config['password']
+        self.token = self.config['token']
+        self.auth = self.config['authorization']
+        self.classifier = self.config['classifier_model']
+        self.detector = self.config['detector_model']
+        self.dev_emails= self.config['dev_emails']
+        self.host = self.config['host']
+        self.classes = self.config['classes']
+        self.model_type= self.config['detector_model_type']
+        self.checkin_interval = self.config['checkin_interval']
+        self.interval = self.config['run_scheduler']
+        self.classifier_model, self.class_list= load_classifier(self.classifier, self.classes)
+        self.detector = load_detector(self.detector, self.model_type)
         #for detect
-        self.EMAIL_ALERTS = bool(self.CONFIG['email_alerts'])
-        self.ER_ALERTS = bool(self.CONFIG['er_alerts'])
-        self.LOG_DIR = self.CONFIG['log_dir']
-        self.CHECKPOINT_F= self.CONFIG['checkpoint_frequency']
-        self.CONFIDENCE = self.CONFIG['confidence']
-        self.TARGETS = self.CONFIG['alert_targets']
-        self.CONSUMER_EMAILS= self.CONFIG['consumer_emails']
-        self.TOKEN = self.CONFIG['token']
+        self.email_alerts = bool(self.config['email_alerts'])
+        self.er_alerts = bool(self.config['er_alerts'])
+        self.log_dir = self.config['log_dir']
+        self.checkpoint_f = self.config['checkpoint_frequency']
+        self.confidence = self.config['confidence']
+        self.targets = self.config['alert_targets']
+        self.consumer_emails = self.config['consumer_emails']
         #for fetch image api
-        self.SAVE_DIR = self.CONFIG['save_dir'] 
-        self.CAMERA_NAMES = dict(self.CONFIG['camera_names'])
-        self.BASE = self.CONFIG['strikeforce_api']
-        self.ACCOUNTS = self.CONFIG['username_scraper']
-        self.AUTH_TOKEN = self.CONFIG['auth_token']
-        self.PATH = self.CONFIG['path']
-        self.PASSWORD_SCRAPER = self.CONFIG['password_scraper']
-
-
-
-
-
+        self.save_dir = self.config['save_dir']
+        self.camera_names = dict(self.config['camera_names'])
+        self.base = self.config['strikeforce_api']
+        self.accounts = self.config['username_scraper']
+        self.auth_token = self.config['auth_token']
+        self.id_path = self.config['id_path']
+        self.password_scraper = self.config['password_scraper']
+        self.traps_path = self.config['camera_traps_path']
