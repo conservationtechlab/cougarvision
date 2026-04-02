0f353b5ffa0ed9fd8b6f977a097b25ed8bd6f042
Montserrat Jara <jaramontse3@gmail.com>
2026-04-02 11:17:06 -0700

    Add attempts to connect to internet to log file

--- cougarvision_utils/get_images.py
+++ cougarvision_utils/get_images.py
@@ -81,12 +81,15 @@ def request_strikeforce(username, auth_token, base, request, parameters):
             return info
 
         except requests.exceptions.ConnectionError as e:
+            logging.warning(f'Failed to connect to internet attempt: {attempt + 1} error: {e}.')
             print(f'Connection Error {attempt + 1}: {e}')
             time.sleep(15) # wait 15 seconds
         except requests.exceptions.Timeout as e:
+            logging.warning(f'Failed to connect to StrikeForce website attempt: {attempt + 1} error {e}.')
             print(f'Timeout Error {attempt + 1}: {e}')
             time.sleep(15) # wait 15 seconds 
 
+    logging.error("Failed to connect after multiple attempts.")
     raise Exception("Failed to connect after multiple attempts.")
 
 
