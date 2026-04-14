62074a691921f4aa80fe7eaa89f7a74e72072852
Montserrat Jara <jaramontse3@gmail.com>
2026-04-14 14:20:03 -0700

    Delete commented out code.

--- fetch_and_alert.py
+++ fetch_and_alert.py
@@ -90,19 +90,6 @@ def main():
     warnings.filterwarnings('ignore', category=FutureWarning) 
     
     logger()
-    """
-    args = parse_args()
-    config_path = args.CONFIG
-    
-    with open(config_path, 'r', encoding='utf-8') as file:
-        config_dict = yaml.safe_load(file)
-
-    # for direct mapping only use fields in ConfigInfo
-    valid_keys = {f.name for f in fields(ConfigInfo)}
-    filtered_keys = {k: v for k, v in config_dict.items() if k in valid_keys}
-
-    config = ConfigInfo(**filtered_keys)
-    """
     config = get_config_info(ConfigInfo)
 
     # pass ConfigInfo dataclass object
