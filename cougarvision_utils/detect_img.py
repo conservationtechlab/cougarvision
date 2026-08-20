441c6605413b00b9314662534a1d4b2e14bfe025
Montserrat Jara <jaramontse3@gmail.com>
2026-08-20 10:47:41 -0700

    Fix regex expression to handle spaces.

--- cougarvision_utils/detect_img.py
+++ cougarvision_utils/detect_img.py
@@ -85,8 +85,9 @@ def detect(images, config):  # pylint: disable=too-many-locals
                 # reset dataframe index
                 cougars = cougars.reset_index(drop=True)
                 # create a row in datafra,e containing only the camera name
-                cougars["cam_name"] = cougars["filepath"
-                                              ].str.extract(r"([A-Z]\d+)")
+                cougars["cam_name"
+                        ] = cougars["filepath"
+                                    ].str.extract(r'([A-Za-z]+[\s_]*\d+)')
                 # Sends alert for each cougar detection
                 for idx in range(len(cougars.index)):
                     label = cougars.at[idx, 'prediction']
